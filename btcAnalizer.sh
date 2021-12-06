a76f52a2c33c5a609e23365f1e719ece39470237
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-12-05 21:03:14 -0500

    Se muestra las transacciones no confirmadas en una tabla.

--- btcAnalizer.sh
+++ btcAnalizer.sh
@@ -17,7 +17,96 @@ trap ctrl_c INT # Activa una funcion cuando se presiona ctrl + c
 function ctrl_c(){
 	echo -e "\n${redColor}[!] Exit... \n${endColor}"
 
+	rm ut.t* 2>/dev/null
 	tput cnorm; exit 1
+}
+
+# Variables globales
+unconfirmed_transactions="https://www.blockchain.com/es/btc/unconfirmed-transactions"
+inspect_tracsaction_url="https://www.blockchain.com/es/btc/tx/"
+inspect_address_url="https://www.blockchain.com/es/btc/address/"
+
+function printTable(){
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    if [[ "${delimiter}" != '' && "$(isEmptyString "${data}")" = 'false' ]]
+    then
+        local -r numberOfLines="$(wc -l <<< "${data}")"
+
+        if [[ "${numberOfLines}" -gt '0' ]]
+        then
+            local table=''
+            local i=1
+
+            for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+            do
+                local line=''
+                line="$(sed "${i}q;d" <<< "${data}")"
+
+                local numberOfColumns='0'
+                numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+                if [[ "${i}" -eq '1' ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+
+                table="${table}\n"
+
+                local j=1
+
+                for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+                do
+                    table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+                done
+
+                table="${table}#|\n"
+
+                if [[ "${i}" -eq '1' ]] || [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+            done
+
+            if [[ "$(isEmptyString "${table}")" = 'false' ]]
+            then
+                echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
+            fi
+        fi
+    fi
+}
+
+function removeEmptyLines(){
+    local -r content="${1}"
+    echo -e "${content}" | sed '/^\s*$/d'
+}
+
+function repeatString(){
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "${string}" != '' && "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
+}
+
+function isEmptyString(){
+    local -r string="${1}"
+
+    if [[ "$(trimString "${string}")" = '' ]]
+    then
+        echo 'true' && return 0
+    fi
+
+    echo 'false' && return 1
+}
+
+function trimString(){
+    local -r string="${1}"
+    sed 's,^[[:blank:]]*,,' <<< "${string}" | sed 's,[[:blank:]]*$,,'
 }
 
 function helpPanel(){
@@ -32,13 +121,26 @@ function helpPanel(){
 	tput cnorm; exit 1
 }
 
-# Variables globales
-unconfirmed_transactions="https://www.blockchain.com/es/btc/unconfirmed-transactions"
-inspect_tracsaction_url="https://www.blockchain.com/es/btc/tx/"
-inspect_address_url="https://www.blockchain.com/es/btc/address/"
 
 function unconfirmedTransactions(){
-	echo "Transacciones no confirmadas."
+	echo '' > ut.tmp
+	while [ "$(cat ut.tmp | wc -l)" == "1" ]; do
+		curl -s "$unconfirmed_transactions" | html2text > ut.tmp
+	done
+
+	hashes=$(cat ut.tmp | grep 'Hash' -A 1 | grep -v -E "Hash|\--|Tiempo")
+
+	echo 'Hash_Cantidad_Bitcoin_Tiempo' > ut.table
+
+	for hash in $hashes; do
+		cantidad=$(cat ut.tmp | grep "$hash" -A 6 | tail -1)
+		bitcoin=$(cat ut.tmp | grep "$hash" -A 4 | tail -1)
+		tiempo=$(cat ut.tmp | grep "$hash" -A 2 | tail -1)
+		echo "${hash}_\$${cantidad::-5}_${bitcoin}_${tiempo}" >> ut.table
+	done
+
+	printTable '_' "$(cat ut.table)"
+
 	tput cnorm
 }
 
